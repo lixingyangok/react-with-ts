98a560f5eff8b5583f1ea5965592b0be655b9436
Lixingyang <176840078@qq.com>
2019-12-19 13:34:37 +0800

    Write chess game

--- src/pages/demo/components/chess/chess.tsx
+++ src/pages/demo/components/chess/chess.tsx
@@ -1,29 +1,101 @@
 import React from 'react';
-import { Box } from './style/chess';
+import { Box, H3 } from './style/chess';
 
 export default class Chess extends React.Component<any, any>{
     constructor(props:any) {
         super(props);
+
         this.state = {
-            player: 'x',
+            player: 'X',
+            step: 0,
+            isOver: false,
+            winner: '',
             arr: [
-                ['x', 'x', 'x'],
-                ['x', 'x', 'x'],
-                ['x', 'x', 'x'],
+                ['', '', ''],
+                ['', '', ''],
+                ['', '', ''],
             ],
         }
     }
     render(){
-        let {state: {arr}} = this;
+        let {
+            state: {
+                arr, step, player, isOver, winner
+            },
+        } = this;
         return <section>
-            <h1>current player：{}</h1>
+            <H3>
+                Current step: {step+1}
+            </H3>
+            <H3>
+                Current player: {player}
+            </H3>
+            <H3 stress >
+                { isOver ? 'Finished' : 'Playing' }
+                { winner }
+                {/*The winner is {123}.*/}
+                &nbsp;&nbsp;&nbsp;
+                <button onClick={()=>this.restart()} >Restart</button>
+            </H3>
             <Box>
-                {arr.map((cur:string, idx:number)=>{
+                {arr.map((cur:string[], idx:number)=>{
                     return <li key={idx}>
-                        {cur}
+                        {cur.map((onePiece:string, idx2:number)=>{
+                            return <span key={idx2} onClick={()=>this.putDown(idx, idx2)} >
+                                {onePiece}
+                            </span>
+                        })}
                     </li>
                 })}
             </Box>
         </section>
+    }
+    putDown( xx:number, yy:number ){
+        const { state } = this;
+        let arr = state.arr;
+        let isOver = this.getIsCover();
+        if( isOver ) return alert('The game has already over!');
+        arr[xx][yy] = state.player;
+        this.setState({
+            ...state,
+            step: state.step + 1,
+            player: ['X', 'O'][ (state.step + 1)%2 ],
+            arr,
+            isOver: this.getIsCover(),
+            winner: this.getResult(),
+        });
+    }
+    restart(){
+        this.setState({
+            ...this.state,
+            step: 0,
+            player: 'X',
+            arr: this.state.arr.map(()=>['', '', '']),
+            isOver: false,
+            winner: '',
+        });
+    }
+    getResult(){
+        const { state: { arr } } = this;
+        let winner = '';
+        for( let [idx, cur] of arr.entries() ){
+
+            winner = cur.reduce((resultStr:string, curStr:string) => {
+                if( resultStr && resultStr==curStr ) return resultStr;
+            });
+            if( winner ) break;
+        }
+        if( !winner ){
+
+        }
+        console.log( winner );
+        return winner;
+    }
+    getIsCover():Boolean{
+        const { state: { arr } } = this;
+        return arr.reduce((result:any[], cur:string[])=>{
+            result.push( cur.every(Boolean) );
+            return result;
+        }, []).every( Boolean );
     }
 }
--- src/pages/demo/components/chess/style/chess.js
+++ src/pages/demo/components/chess/style/chess.js
@@ -1,13 +1,33 @@
 import styled from 'styled-components';
 
-
-const size = '450px';
+const oneBlock = 100;
 export const Box = styled.ul`
-    width: ${size};
-    height: ${size};
-    background: #eaeaea;
-    border: solid 1px #ccc;
+    width: ${oneBlock*3}px;
+    padding: 0;
+    background: #f1f1f1;
+    border: solid 1px #ddd;
     margin: 0 auto;
+    >li{
+        height: calc(100% / 3);
+        display: flex;
+    }
+    span{
+        width: ${oneBlock}px;
+        height: ${oneBlock}px;
+        line-height: ${oneBlock}px;
+        vertical-align: -0.1em;
+        font-size: 20px;
+        transition: 0.2s;
+        cursor: pointer;
+        outline: solid 1px #ddd;
+        &:hover{
+            background: rgba(0,0,0,0.05);
+        }
+    }
 `;
 
-
+export const H3 = styled.h3`
+	line-height: 1;
+	margin: 10px 0;
+	color: ${ pp => pp.stress ? 'red' : '' };
+`
