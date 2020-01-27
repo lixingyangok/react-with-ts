e11e46b8008d058230db61e6c65ada88269c7eb9
lixingyang <176840078@qq.com>
2020-01-27 20:59:29 +0800

    初步完成-结构与行为的分享

--- src/pages/demo/components/todolistSeparation/interface.tsx
+++ src/pages/demo/components/todolistSeparation/interface.tsx
@@ -3,38 +3,38 @@ import cpnt from './style/style.js';
 
 interface IProps {
     list: string[],
+    inputing: string,
     fnInputing:Function,
     fnDel:Function,
+    fnAdd:Function,
 }
 
-export default class extends React.Component<IProps>{
-    constructor( props:IProps ){
-        super(props);
-    }
-    render(){
-        const { list, fnInputing, fnDel } = this.props;
-        return <cpnt.Wrap>
-            <h1>
-                This todolist has splited the UI and event.
-            </h1>
-            <cpnt.InputWrap>
-                <input onChange={ ev=>fnInputing(ev.target.value) } />
-                <button>Submit</button>
-            </cpnt.InputWrap>
-            <cpnt.Ul>
-                {list.map((cur,idx)=>{
-                    return <li key={idx} >
-                        {idx+1}. {cur}
-                        &nbsp;&nbsp;&nbsp;
-                        {/*  */}
-                        <button  style={{padding: '3px 6px', lineHeight: '10px' }}
-                            onClick={()=>fnDel(idx)}
-                        >
-                            Delete
-                        </button>
-                    </li>;
-                })}
-            </cpnt.Ul>
-        </cpnt.Wrap>;
-    }
+
+export default function( props:IProps ){
+    const { list, inputing, fnInputing, fnDel, fnAdd } = props;
+    return <cpnt.Wrap>
+        <h1>
+            This todolist has splited the UI and event.
+        </h1>
+        <cpnt.InputWrap>
+            <input value={inputing} onChange={ ev=>fnInputing(ev.target.value) } />
+            <button onClick={()=>fnAdd()} >
+                Submit
+            </button>
+        </cpnt.InputWrap>
+        <cpnt.Ul>
+            {list.map((cur,idx)=>{
+                return <li key={idx} >
+                    {idx+1}. {cur}
+                    &nbsp;&nbsp;&nbsp;
+                    {/*  */}
+                    <button  style={{padding: '3px 6px', lineHeight: '10px' }}
+                        onClick={()=>fnDel(idx)}
+                    >
+                        Delete
+                    </button>
+                </li>;
+            })}
+        </cpnt.Ul>
+    </cpnt.Wrap>;
 }
--- src/pages/demo/components/todolistSeparation/todolistSeparation.tsx
+++ src/pages/demo/components/todolistSeparation/todolistSeparation.tsx
@@ -13,18 +13,20 @@ interface IState { //指定 state 内容
 }
 
 export default class ToDoList extends React.Component<IProps, IState>{
-    
     constructor(props:IProps){
         super(props);
         this.state = store.getState();
         // ▼监听store变化、必须用箭头函数
-        store.subscribe(  ()=>this.setState( store.getState() ) );
+        store.subscribe(()=>{
+            this.setState(store.getState());
+        });
     }
     render(){
-        const { list } = this.state;
-        return <Interface list={list} 
+        const { list, inputing } = this.state;
+        return <Interface list={list} inputing={inputing}
             fnInputing={this.fnInputing.bind(this)}
             fnDel={this.fnDel.bind(this)}
+            fnAdd={this.fnAdd.bind(this)}
         />
     }
     fnInputing( newVal:string ){
@@ -32,5 +34,8 @@ export default class ToDoList extends React.Component<IProps, IState>{
     }
     fnDel( idx:number ){
         store.dispatch( TO_REMOVE(idx) );
+    }
+    fnAdd(newVal:string){
+        store.dispatch(TO_ADD(this.state.inputing))
     }
 }
