264e417223027705b0022919b13776ac458e7e30
lixingyang <176840078@qq.com>
2020-02-12 20:07:26 +0800

    李：添加注释

--- src/pages/hook/children/useState/useState.tsx
+++ src/pages/hook/children/useState/useState.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 export default function(){
+    // ▼下列 React.useState 的调用顺序，数量必须恒定，不可通过判断产生变化
     const [number, setNumber] = React.useState(0);
     return <div>
         <h2>Learn useState by this demo.</h2>
