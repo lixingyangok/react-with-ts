416dfc0629b5161de7adf8a8b1ae6bd639262fc4
lixingyang <176840078@qq.com>
2020-02-20 13:32:04 +0800

    李：学习useRef

--- src/pages/hook/children/useRef/useRef.tsx
+++ src/pages/hook/children/useRef/useRef.tsx
@@ -1,48 +1,41 @@
 import React from 'react';
-import styled from 'styled-components';
+
+const inputStyle = {
+    display: 'inline-block',
+    width: '300px',
+    padding: '0.2em 1.5em',
+    margin: '0 auto',
+    border: 'solid 1px #aaa',
+}
 
 export default function(){
     const [time, setTime] = React.useState(new Date());
     const [text, setText] = React.useState('value');
-    const Time = `
-        ${time.toLocaleString()} ${time.getMilliseconds()}
-    `.trim();
-    const Input = styled.input`
-        width: 300px;
-        padding: 0.5em 1.5em;
-        text-align: center;
-        &:focus{
-            outline: solid 1px blue;
-        }
-    `;
     const myRef:any = React.useRef(null);
-    const myIpt:any = React.useRef(null);
     const myIpt02:any = React.useRef(null);
-    // React.useEffect(
-    //     ()=>{
-    //         myIpt02.current.value = text;
-    //         console.log('组件变化');
-    //     },
-    //     [time]
-    // );
     const UpdateTime = function(){
         setTime(new Date());
         myRef.current.innerText = Time;
     }
     const beFocused = function(){
-        myIpt.current.focus();
+        myIpt02.current.focus();
     }
+    const Time = `${time.toLocaleString()} - ${time.getMilliseconds()}`;
+    console.log('重新渲染了');
     return <div>
         <h2>Learn useRef by this demo.</h2>
-        <Input ref={myIpt} value={Time} readOnly />
-        <Input ref={myIpt02} value={text} onChange={(ev:any)=>setText(ev.target.value)} />
-        <p ref={myRef}>&nbsp;&nbsp;</p>
         <br/>
-        <button onClick={()=>UpdateTime()} >
-            Update the time, and make it focused.
-        </button>
-        <button onClick={()=>beFocused()} >
-            Be focused.
-        </button>
+        <div>
+            <p ref={myRef} style={inputStyle}>&nbsp;&nbsp;</p>
+            <button onClick={()=>UpdateTime()} >
+                Update the time.
+            </button>
+        </div>
+        <div>
+            <input style={inputStyle} ref={myIpt02} value={text} onChange={(ev:any)=>setText(ev.target.value)} /> 
+            <button onClick={()=>beFocused()} >
+                Make it focused.
+            </button>
+        </div>
     </div>;
 };
