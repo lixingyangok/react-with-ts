63f7a5d7a481eddf0d4e01b9b20d4e13a5cd6f41
lixingyang <176840078@qq.com>
2020-03-02 19:51:34 +0800

    删除无用数据

--- src/pages/demo-2/demo-2.tsx
+++ src/pages/demo-2/demo-2.tsx
@@ -1,50 +1,17 @@
 import React from 'react';
 import { Menu } from 'antd';
 
-// class Abc extends React.Component {
-//     state = {
-//         current: 'mail',
-//     };
-//     handleClick = (ev:any) => {
-//         console.log('click ', ev);
-//         this.setState({
-//             current: ev.key,
-//         });
-//     };
-//     render () {
-//         console.log(Menu.Item);
-//         return <div className="center-box">
-//             asdf
-//             <Menu onClick={this.handleClick} selectedKeys={[this.state.current]} mode="horizontal">
-//                 <Menu.Item key="mail">
-//                     Navigation One
-//                 </Menu.Item>
-//                 {/*<Menu.Item key="alipay">
-//                     <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-//                         Navigation Four - Link
-//                     </a>
-//                 </Menu.Item> */}
-//             </Menu>
-//         </div>
-//     };
-// }
-
-export default function (){
+export default function Abc (){
     const [current, setCurrent] = React.useState('mail');
     const handleClick = ()=>{
         console.log('handleClick', setCurrent);
     }
     return <div className="center-box">
-        asdf
-        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal">
-            {/* <Menu.Item key="mail">
-                Navigation One
-            </Menu.Item> */}
-            {/*<Menu.Item key="alipay">
-                <a href="https://ant.design" target="_blank" rel="noopener noreferrer">
-                    Navigation Four - Link
-                </a>
-            </Menu.Item> */}
+        <Menu onClick={handleClick} selectedKeys={[current]} mode="horizontal" theme="dark">
+            <Menu.Item key="mail" >菜单项</Menu.Item>
+            <Menu.Item key="mail1" >菜单项</Menu.Item>
+            <Menu.Item key="mail2" >菜单项</Menu.Item>
+            <Menu.Item key="mail3" >菜单项</Menu.Item>
         </Menu>
     </div>
 }
