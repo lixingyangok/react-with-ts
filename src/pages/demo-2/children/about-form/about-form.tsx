7d4e186908be8c09b8e089e2d85092198e118152
lixingyang <176840078@qq.com>
2020-03-19 20:41:03 +0800

    李：提交form

--- src/pages/demo-2/children/about-form/about-form.tsx
+++ src/pages/demo-2/children/about-form/about-form.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
 import cpnt from './style/about-form';
 
+interface IForm {
+    name: string,
+    sex: number,
+    hobbies: Array<any>,
+    marriage: number,
+}
+
 export default function(){
     const [form, setForm] = React.useState({
         name: 'Tom',
         sex: 0,
-        hobbies: '',
+        hobbies: [],
         marriage: 1,
     });
     const toChangeVal = function(ev:any ){
@@ -15,8 +22,16 @@ export default function(){
         });
     }
     const toChangeCheckBox = function (ev:any) {
-        const {target} = ev;
-        console.log(target.checked);
+        const { target:{ checked, value } } = ev;
+        let hobbies:Array<number> = form.hobbies;
+        console.log(value);
+        if(checked){
+            hobbies.find(cur=>cur==value) || hobbies.push(value);
+        }else{
+            hobbies = hobbies.filter(cur=>cur!=value);
+        }
+        // setForm({ ...form, hobbies });
+        // 无法进行，暂时搁置
     }
     const showForm = function(){
         console.log( 1 );
@@ -30,10 +45,10 @@ export default function(){
             <div>
                 gender：
                 <label>
-                    male <input value={0} defaultChecked={form.sex===0} name="sex" type="radio"  onChange={toChangeVal} />
+                    male <input value={0} defaultChecked={form.sex===0} name="sex" type="radio" onChange={toChangeVal} />
                 </label>
                 <label>
-                    female <input value={1} defaultChecked={form.sex===1} name="sex" type="radio"  onChange={toChangeVal} />
+                    female <input value={1} defaultChecked={form.sex===1} name="sex" type="radio" onChange={toChangeVal} />
                 </label>
             </div>
             <div>
@@ -46,10 +61,12 @@ export default function(){
             <div>
                 hobbies:
                 <label>
-                    reading <input value={11} defaultChecked={form.sex===11} name="hobbies" type="checkbox"  onChange={toChangeCheckBox} />
+                    reading
+                    <input value={11} defaultChecked={form.sex===11} name="hobbies" type="checkbox" onChange={toChangeCheckBox} />
                 </label>
                 <label>
-                    exercising <input value={22} defaultChecked={form.sex===22} name="hobbies" type="checkbox"  onChange={toChangeCheckBox} />
+                    exercising
+                    <input value={22} defaultChecked={form.sex===22} name="hobbies" type="checkbox" onChange={toChangeCheckBox} />
                 </label>
             </div>
             <div>
